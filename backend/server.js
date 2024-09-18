61105f3d78ce1283a58bee90db8197bede82c3f7
Sayak Bhattacharya <sayakofficial21@gmail.com>
2024-09-18 23:25:41 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -11,14 +11,14 @@ const FRONTEND_URL = process.env.FRONTEND_URL;
 const User = require("./models/user&noteModel.js");
 
 app.use(express.json());
-/*app.use(
+app.use(
   cors({
     origin: FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
-);*/
-app.use(cors());
+);
+//app.use(cors());
 
 mongoose
   .connect(MONGODB_URl)
