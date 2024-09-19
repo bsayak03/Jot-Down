8069e98bf937878833da36ad0db12af65a4e5e5e
Sayak Bhattacharya <sayakofficial21@gmail.com>
2024-09-19 11:54:02 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 
 require("dotenv").config();
 const PORT = process.env.PORT;
-const MONGODB_URL = process.env.MONGODB_URl;
+const MONGODB_URL = process.env.MONGODB_URL;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
 const User = require("./models/user&noteModel.js");
