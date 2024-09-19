839c9381a3531f91773c6b87f6c891cfc8b47d5a
Sayak Bhattacharya <sayakofficial21@gmail.com>
2024-09-19 11:25:50 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,13 +9,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"), // Alias '@' points to 'src'
     },
   },
-  server: {
-    proxy: {
-      "/api": {
-        target: process.env.VITE_REACT_APP_BACKEND_BASEURL, // Fallback if the env var is missing
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
